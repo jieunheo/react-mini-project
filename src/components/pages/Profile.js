3719ebddfb151dd1a9552cbe207929c7b85b5b89
heojieun <gjekdmlwl@naver.com>
2022-07-04 15:02:15 +0900

    profile page

---
+++ src/components/pages/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './Profile.module.css';
+
+const Profile = () => {
+  
+
+  return (
+    <main className={classes.main}>
+      Profile page
+    </main>
+  );
+}
+
+export default Profile;
