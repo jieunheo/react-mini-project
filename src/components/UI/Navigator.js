4ccc0b3a47cf8a898a227394076e2d88f0958d4f
heojieun <gjekdmlwl@naver.com>
2022-07-04 17:22:58 +0900

    현재 페이지인 경우 네비게이션 스타일 주기

--- src/components/UI/Navigator.js
+++ src/components/UI/Navigator.js
@@ -1,39 +1,46 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import classes from './Navigator.module.css';
 
 const Navigator = ({ login }) => {
+  const location = useLocation();
+  const pathName = location.pathname;
   return (
     <nav className={classes.nav}>
       <ul>
         <li>
           <Link to="/">
-            <a>Home</a>
+            <a className={pathName === '/' && classes.path}>Home</a>
           </Link>
         </li>
         <li>
           <Link to="/movies">
-            <a>Movies</a>
+            <a className={pathName === '/movies' && classes.path}>Movies</a>
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">
+            <a className={pathName === '/about' && classes.path}>About</a>
           </Link>
         </li>
         {
           !login ? (
             <li>
               <Link to="/login">
-                <a>Login</a>
+                <a className={pathName === '/login' && classes.path}>Login</a>
               </Link>
             </li>
           ) : (
             <>
               <li>
                 <Link to="/todo">
-                  <a>To Do List</a>
+                  <a className={pathName === '/todo' && classes.path}>To Do List</a>
                 </Link>
               </li>
               <li>
                 <Link to="/profile">
-                  <a>Profile</a>
+                  <a className={pathName === '/profile' && classes.path}>Profile</a>
                 </Link>
               </li>
             </>
