160f5f098f7d74ce861e54ec9bdd3cd14e40a9d2
heojieun <gjekdmlwl@naver.com>
2022-07-04 15:02:38 +0900

    새로 추가한 페이지 적용

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import Navigator from './components/UI/Navigator';
 import Home from './components/pages/Home';
 import ToDo from './components/pages/ToDo';
+import Login from './components/pages/Login';
+import Profile from './components/pages/Profile';
+import Movies from './components/pages/Movies';
 
 function App() {
+  const [login, setLogin] = useState(false);
+
   return (
     <Router>
-      <Navigator />
+      <Navigator login={login} />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/todo' element={<ToDo />} />
+        <Route path='/movies' element={<Movies />} />
+        {!login && <Route path='/Login' element={<Login setLogin={setLogin(true)} />} />}
+        {!login && <Route path="*" element={<Navigate to="/login" setLogin={setLogin(true)} />} />}
+        {login && <Route path='/todo' element={<ToDo />} />}
+        {login && <Route path="/profile" element={<Profile />} />}
       </Routes>
     </Router>
   );
--- src/components/UI/Navigator.js
+++ src/components/UI/Navigator.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import classes from './Navigator.module.css';
 
-const Navigator = () => {
+const Navigator = ({ login }) => {
   return (
     <nav className={classes.nav}>
       <ul>
@@ -13,10 +13,32 @@ const Navigator = () => {
           </Link>
         </li>
         <li>
-          <Link to="/todo">
-            <a>To Do List</a>
+          <Link to="/movies">
+            <a>Movies</a>
           </Link>
         </li>
+        {
+          !login ? (
+            <li>
+              <Link to="/login">
+                <a>Login</a>
+              </Link>
+            </li>
+          ) : (
+            <>
+              <li>
+                <Link to="/todo">
+                  <a>To Do List</a>
+                </Link>
+              </li>
+              <li>
+                <Link to="/profile">
+                  <a>Profile</a>
+                </Link>
+              </li>
+            </>
+          )
+        }
       </ul>
     </nav>
   )
