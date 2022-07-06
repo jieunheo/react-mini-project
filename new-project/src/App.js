88e91bb367edbc87aa53886103f74c0347168433
heojieun <gjekdmlwl@naver.com>
2022-07-06 15:52:14 +0900

    class 컴포넌트에서 api 가져오기

--- new-project/src/App.js
+++ new-project/src/App.js
@@ -1,9 +1,33 @@
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class App extends Component {
+	constructor (props) {
+		super(props);
+
+		// 상태관리 할 state
+		this.state = {
+			users: []
+		}
+	}
+
+	// component가 mount 된 직후 실행
+	componentWillMount() {
+    axios('https://api.randomuser.me/?nat=US&results=5')
+      .then(response => this.setState({
+        users: response.data.results
+      }));
+	}
+
+	render() {
+		return (
+			<div>
+				{
+          this.state.users.map(user => <p>{user.email}</p>)
+        }
+			</div>
+		);
+	};
 }
 
 export default App;
