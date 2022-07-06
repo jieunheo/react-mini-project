5a05f744399d7a745d71d186c3cd8bdbe228b357
heojieun <gjekdmlwl@naver.com>
2022-07-06 15:55:28 +0900

    loading 상태 추가

--- new-project/src/App.js
+++ new-project/src/App.js
@@ -7,24 +7,38 @@ class App extends Component {
 
 		// 상태관리 할 state
 		this.state = {
-			users: []
+			users: [],
+			loading: false
 		}
+	}
+  
+  // api에서 user 값을 가져온다.
+	getUser() {
+		this.setState({
+			loading: true
+		});
+		axios('https://api.randomuser.me/?nat=US&results=5')
+			.then(response => this.setState({
+				users: response.data.results,
+        loading: false
+			}));
 	}
 
 	// component가 mount 된 직후 실행
 	componentWillMount() {
-    axios('https://api.randomuser.me/?nat=US&results=5')
-      .then(response => this.setState({
-        users: response.data.results
-      }));
+    this.getUser();
 	}
 
 	render() {
 		return (
 			<div>
-				{
-          this.state.users.map(user => <p>{user.email}</p>)
-        }
+        {this.state.loading && <p>Loading...</p>}
+				{!this.state.loading && this.state.users.map(user => (
+					<div key={user.id.value}>
+						<h5>{user.name.first}</h5>
+						<p>{user.email}</p>
+					</div>
+				))}
 			</div>
 		);
 	};
