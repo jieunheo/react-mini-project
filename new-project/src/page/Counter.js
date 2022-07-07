4850440b91344658a6159a945b0338215dfa31ed
heojieun <gjekdmlwl@naver.com>
2022-07-07 10:31:58 +0900

    함수형 컴포넌트에서 useEffect로 같은 기능 구현

--- new-project/src/page/Counter.js
+++ new-project/src/page/Counter.js
@@ -1,68 +1,72 @@
-import { Component, useState } from "react";
+import { Component, useEffect, useState } from "react";
 
 
-// const Counter = () => {
-//   const [count, setCount] = useState(0);
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Hello, ${count}~`;
+  });
   
-//   const increment = () => {
-//     setCount(preCount => preCount + 1);
-//   }
-  
-//   const decrement = () => {
-//     setCount(preCount => preCount - 1);
-//   }
-
-//   return (
-//     <div>
-//       <h1>Counter Page</h1>
-//       <h2>count: {count}</h2>
-//       <div>
-//         <button type="button" onClick={increment}>UP</button>
-//         <button type="button" onClick={decrement}>DOWN</button>
-//       </div>
-//     </div>
-//   )
-// }
-
-class Counter extends Component {
-  state = {
-    count: 0
+  const increment = () => {
+    setCount(preCount => preCount + 1);
   }
   
-  increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
-  }
-  
-  decrement = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
+  const decrement = () => {
+    setCount(preCount => preCount - 1);
   }
 
-  // 처음 마운트 시
-  componentDidMount() {
-    document.title = `Hello, ${this.state.count}~`;
-  }
-
-  // 상태 값 수정 시
-  componentDidUpdate() {
-    document.title = `Hello, ${this.state.count}~`;
-  }
-
-  render() {
-    return (
+  return (
+    <div>
+      <h1>Counter Page</h1>
+      <h2>count: {count}</h2>
       <div>
-        <h1>Counter Page</h1>
-        <h2>count: {this.state.count}</h2>
-        <div>
-          <button type="button" onClick={this.increment}>UP</button>
-          <button type="button" onClick={this.decrement}>DOWN</button>
-        </div>
+        <button type="button" onClick={increment}>UP</button>
+        <button type="button" onClick={decrement}>DOWN</button>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+// class Counter extends Component {
+//   state = {
+//     count: 0
+//   }
+  
+//   increment = () => {
+//     this.setState({
+//       count: this.state.count + 1
+//     });
+//   }
+  
+//   decrement = () => {
+//     this.setState({
+//       count: this.state.count - 1
+//     });
+//   }
+
+//   // 처음 마운트 시
+//   componentDidMount() {
+//     document.title = `Hello, ${this.state.count}~`;
+//   }
+
+//   // 상태 값 수정 시
+//   componentDidUpdate() {
+//     document.title = `Hello, ${this.state.count}~`;
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Counter Page</h1>
+//         <h2>count: {this.state.count}</h2>
+//         <div>
+//           <button type="button" onClick={this.increment}>UP</button>
+//           <button type="button" onClick={this.decrement}>DOWN</button>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
 
 export default Counter;
