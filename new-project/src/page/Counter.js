ec7c927691de31410f45edc4f5775166c5fcd180
heojieun <gjekdmlwl@naver.com>
2022-07-07 10:05:04 +0900

    class component에서 만든 counter

--- new-project/src/page/Counter.js
+++ new-project/src/page/Counter.js
@@ -1,11 +1,32 @@
 import { Component } from "react";
 
 
-class Counter extends Component () {
+class Counter extends Component {
+  state = {
+    count: 0
+  }
+  
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+  
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Counter Page</h1>
+        <h2>count: {this.state.count}</h2>
+        <div>
+          <button type="button" onClick={this.increment}>UP</button>
+          <button type="button" onClick={this.decrement}>DOWN</button>
+        </div>
       </div>
     )
   }
